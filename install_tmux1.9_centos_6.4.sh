e6c04e8f91204f3aa4850627071d826ed66f5913
carlba <genzorg@gmail.com>
2016-02-12 10:40:27 +0100

    Update install_tmux1.9_centos_6.4.sh

--- install_tmux1.9_centos_6.4.sh
+++ install_tmux1.9_centos_6.4.sh
@@ -1,4 +1,3 @@
-
 function install_plugins {
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   rm -rf ~/.tmux.conf
@@ -10,34 +9,30 @@ function install_tmuxp {
   pip install tmuxp
 }
 
-$(tmux -V | grep -q 1.9) && echo "Tmux 1.9 already installed" && install_plugins && install_tmuxp && exit
+$(tmux -V | grep -q 2.0) && echo "Tmux 2.0 already installed" && install_plugins && install_tmuxp && exit
 
 pkill tmux
 yum -y remove tmux
 yum -y install ncurses-devel gcc git
 mkdir install
 cd install
-wget http://downloads.sourceforge.net/tmux/tmux-1.9.tar.gz
-curl -OL https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+wget http://downloads.sourceforge.net/project/tmux/tmux/tmux-2.0/tmux-2.0.tar.gz
+curl -OL https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
 
-tar -xzvf libevent-2.0.21-stable.tar.gz
-tar -xzvf tmux-1.9.tar.gz
+tar -xzvf libevent-2.0.22-stable.tar.gz
+tar -xzvf tmux-2.0.tar.gz
 
 cd libevent-2*
 ./configure --prefix=/usr/local
 make && make install
 
-cd ../tmux-1.9
+cd ../tmux-2.0
 LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
 make && make install
 cd ~
 ln -s /usr/local/bin/tmux /usr/bin/tmux
 
-rm -f libevent-2.0.21-stable.tar.gz
-rm -f tmux-1.8.tar.gz
-rm -rf tmux-1.8*
+rm -f libevent-2.0.22-stable.tar.gz
+rm -f tmux-2.0.tar.gz
+rm -rf tmux-2.0*
 rm -rf libevent-2.0*
-
-
-
-
